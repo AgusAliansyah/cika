d10dc22d8a1f6d7ea25f620599662970daa04e48
Agus Aliansyah <70519086+AgusAliansyah@users.noreply.github.com>
2021-02-21 21:09:37 +0700

    Add files via upload

---
+++ cika/makermenu.js
@@ -0,0 +1,49 @@
+const makermenu = (prefix) => { 
+	return ` 
+╭───「 *CREATOR MENU* 」───
+│
+├➲ *${prefix}sticker*
+├➲ *${prefix}quotemaker [tx/wtrmk/tema]*
+├➲ *${prefix}nulis [nama/kelas/text]*
+├➲ *${prefix}trigger [reply image]*
+├➲ *${prefix}rip [reply image]*
+├➲ *${prefix}wasted [reply image]*
+├➲ *${prefix}cphlogo [Agus/Nesta]*
+├➲ *${prefix}cglitch [Agus/Nesta]*
+├➲ *${prefix}cpubg [Agus/Nesta]*
+├➲ *${prefix}cml [Cika/Agus]*
+├➲ *${prefix}tahta [Agus]*
+├➲ *${prefix}croman [Agus dan Cika]*
+├➲ *${prefix}cthunder [Agus]*
+├➲ *${prefix}cbpink [Agus]*
+├➲ *${prefix}cmwolf [Agus]*
+├➲ *${prefix}csky [Agus]*
+├➲ *${prefix}cwooden [Agus]*
+├➲ *${prefix}cflower [Agus]*
+├➲ *${prefix}clove [Agus]*
+├➲ *${prefix}ccrossfire [Agus]*
+├➲ *${prefix}cnaruto [Agus]*
+├➲ *${prefix}cparty [Agus]*
+├➲ *${prefix}cshadow [Agus]*
+├➲ *${prefix}cminion [Agus]*
+├➲ *${prefix}cneon [Agus]*
+├➲ *${prefix}cneon2 [Agus]*
+├➲ *${prefix}cneongreen [Agus]*
+├➲ *${prefix}c3d [Agus]*
+├➲ *${prefix}csky [Agus]*
+├➲ *${prefix}tts [id Agus]*
+├➲ *${prefix}ttp [Agus]*
+├➲ *${prefix}slide [Agus]*
+├➲ *${prefix}stiker*
+├➲ *${prefix}gifstiker*
+├➲ *${prefix}toimg*
+├➲ *${prefix}img2url*
+├➲ *${prefix}nobg*
+├➲ *${prefix}tomp3*
+├➲ *${prefix}ocr*
+│
+╰──────────────────────
+
+	           *©Agus Nesta*`
+	}
+exports.makermenu = makermenu
